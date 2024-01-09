9b4d7b4843bc47fb936586fcad403b8f54f6df3b
Morelzy <technicalzfg@gmail.com>
2024-01-09 11:23:45 +0100

    Document list

--- components/documents/item.tsx
+++ components/documents/item.tsx
@@ -1,12 +1,17 @@
 'use client'
 
 import type { Id } from '@/convex/_generated/dataModel'
-import { ChevronDown, ChevronRight, type LucideIcon } from 'lucide-react'
+import { useRouter } from 'next/navigation'
+import { ChevronDown, ChevronRight, Plus, type LucideIcon } from 'lucide-react'
+import { useMutation } from 'convex/react'
+import { toast } from 'sonner'
 
+import { api } from '@/convex/_generated/api'
 import { cn } from '@/lib/utils'
+import { Skeleton } from '@/components/ui/skeleton'
 
 interface ItemsProps {
-  id?: Id<'document'>
+  id?: Id<'documents'>
   documentIcon?: string
   active?: boolean
   expanded?: boolean
@@ -14,6 +19,7 @@ interface ItemsProps {
   level?: number
   icon: LucideIcon
   label: string
+  onExpand?: () => void
   onClick: () => void
 }
 
@@ -27,7 +33,37 @@ export const Item = ({
   expanded,
   isSearch,
   level = 0,
+  onExpand,
 }: ItemsProps) => {
+  const router = useRouter()
+  const create = useMutation(api.documents.create)
+
+  const handleExpand = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) => {
+    event.stopPropagation()
+    onExpand?.()
+  }
+
+  const onCreate = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    event.stopPropagation()
+    if (!id) return
+    const promise = create({ title: 'Untitled', parentDocument: id }).then(
+      (documentId) => {
+        if (!expanded) {
+          onExpand?.()
+        }
+        // router.push(`/documents/${documentId}`)
+      }
+    )
+
+    toast.promise(promise, {
+      loading: 'Creating a new note...',
+      success: 'New note created!',
+      error: 'Failed to create a new note.',
+    })
+  }
+
   const ChevronIcon = expanded ? ChevronDown : ChevronRight
 
   return (
@@ -44,7 +80,7 @@ export const Item = ({
         <div
           role="button"
           className="h-full rounded-sm hover:bg-neutral-300 mr-1"
-          onClick={() => {}}
+          onClick={handleExpand}
         >
           <ChevronIcon className="h-4 w-4 shrink-0 text-muted-foreground/50" />
         </div>
@@ -60,6 +96,31 @@ export const Item = ({
           <span className="text-xs">Ctrl</span>K
         </kbd>
       )}
+      {!!id && (
+        <div className="ml-auto flex items-center gap-x-2">
+          <div
+            role="button"
+            onClick={onCreate}
+            className="opacity-0 group-hover:opacity-100 h-full ml-auto rounded-3xl hover:bg-neutral-300"
+          >
+            <Plus className="h-4 w-4 text-muted-foreground" />
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+Item.Skeleton = function ItemSkeleton({ level }: { level?: number }) {
+  return (
+    <div
+      style={{
+        paddingLeft: level ? `${level * 12 + 25}px` : '12px',
+      }}
+      className="flex gap-x-2 py-[3px]"
+    >
+      <Skeleton className="h-4 w-4" />
+      <Skeleton className="h-4 w-[30%]" />
     </div>
   )
 }
--- components/documents/navigation.tsx
+++ components/documents/navigation.tsx
@@ -10,18 +10,18 @@ import {
   Settings,
 } from 'lucide-react'
 import { useMediaQuery } from 'usehooks-ts'
-import { useMutation, useQuery } from 'convex/react'
+import { useMutation } from 'convex/react'
 import { toast } from 'sonner'
 
 import { api } from '@/convex/_generated/api'
 import { cn } from '@/lib/utils'
 import { UserItem } from '@/components/documents/user-item'
 import { Item } from '@/components/documents/item'
+import { DocumentList } from '@/components/documents/document-list'
 
 export const Navigation = () => {
   const pathname = usePathname()
   const isMobile = useMediaQuery('(max-width: 768px)')
-  const documents = useQuery(api.documents.get)
   const create = useMutation(api.documents.create)
 
   const isResizingRef = useRef(false)
@@ -139,9 +139,7 @@ export const Navigation = () => {
           <Item onClick={handleCreate} label="New page" icon={PlusCircle} />
         </div>
         <div className="mt-4">
-          {documents?.map((document) => (
-            <p key={document._id}>{document.title}</p>
-          ))}
+          <DocumentList />
         </div>
         <div
           onMouseDown={handleMouseDown}
