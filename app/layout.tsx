0d4f87a59133a4d3143b63465a7ce2556f447a3d
Morelzy <technicalzfg@gmail.com>
2024-01-11 12:04:37 +0100

    Settings modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from 'next/font/google'
 import { Toaster } from 'sonner'
 
 import { ConvexClientProvider } from '@/components/providers/convex-provider'
+import { ModalProvider } from '@/components/providers/modal-provider'
 
 import './globals.css'
 
@@ -38,6 +39,7 @@ export default function RootLayout({
       <html lang="en">
         <body className={inter.className}>
           <Toaster position="bottom-center" />
+          <ModalProvider />
           {children}
         </body>
       </html>
--- components/documents/navigation.tsx
+++ components/documents/navigation.tsx
@@ -27,9 +27,11 @@ import {
 } from '@/components/ui/popover'
 import { TrashBox } from '@/components/documents/trash-box'
 import { useSearch } from '@/hooks/use-search'
+import { useSettings } from '@/hooks/use-settings'
 
 export const Navigation = () => {
   const search = useSearch()
+  const settings = useSettings()
   const pathname = usePathname()
   const isMobile = useMediaQuery('(max-width: 768px)')
   const create = useMutation(api.documents.create)
@@ -145,7 +147,7 @@ export const Navigation = () => {
         <div>
           <UserItem />
           <Item label="Search" icon={Search} isSearch onClick={search.onOpen} />
-          <Item label="Settings" icon={Settings} onClick={() => {}} />
+          <Item label="Settings" icon={Settings} onClick={settings.onOpen} />
           <Item onClick={handleCreate} label="New page" icon={PlusCircle} />
         </div>
         <div className="mt-4">
